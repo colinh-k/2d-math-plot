a9c3cd154ffec91d3a46c44c15ea1d8d6711376d
Colin <colinh.k@icloud.com>
2021-04-24 15:21:47 -0700

    added scaling axes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,84 +1,46 @@
 const equationField = document.getElementById('equation-field');
-const canvas = document.getElementById('graph-window');
-const ctx = canvas.getContext('2d');
+// const canvas = document.getElementById('graph-window');
+// const ctx = canvas.getContext('2d');
 
 // variables for math parsing 
-let expression = '';
+let expression = 'sin(x)';
 let scope = {
     x: 0,
 }
+let graph;
 
-// number of line segments
-const n = 1000;
-
-// graph window
-let xMin = -10, xMax = 10, yMin = -10, yMax = 10;
-
-// grid size in px
-const gridStep = 55;
-
-// how many lines from canvas origin to place x axis
-const xAxisOffset = Math.round(canvas.height / gridStep / 2);
-const xAxisStartLabel = 1;
-
-// how many lines from canvas origin to place y axis
-const yAxisOffset = Math.round(canvas.width / gridStep / 2);
-const yAxisStartLabel = 1;
-
-ctx.translate(yAxisOffset * gridStep, xAxisOffset * gridStep);
-initExprFromHash();
-plot();
-// animate();
+// initExprFromHash();
 
 // listen for changes to text input field 
 
-equationField.value = expression;
-equationField.addEventListener('keyup', event => {
-    expression = equationField.value;
-    setHashFromExpr();
-    plot();
-});
-
 // update from back and forward buttons
-window.addEventListener('hashchange', initExprFromHash);
+// window.addEventListener('hashchange', initExprFromHash);
 
-function plot() {
-    let xPixel, yPixel;
-    // these vary between 0 and 1, with a step defined by n
-    let percentX, percentY;
-    // math space coordinates, vary between min and max of graph window
-    let mathX, mathY;
+window.onload = function () {  
+    graph = new Graph({  
+        canvasId: "graph-window",  
+        minX: -10,  
+        minY: -10,  
+        maxX: 10,  
+        maxY: 10,  
+        unitsPerTick: 1  
+    });  
     
-    // clear the canvas
-    ctx.clearRect(0,0,canvas.width, canvas.height);    
+    graph.drawEquation((x) => {
+        return evaluateFunction(x);
+    }, "black", 3);
     
-    drawGrid();
-    // ctx.translate(yAxisOffset * gridStep, xAxisOffset * gridStep);
-
-    ctx.beginPath();
-    // og: for (let i = 0; i < n; i++) {
-    for (let i = 0; i < n; i++) {
-        percentX = i / (n - 1);
-        // mathX varies between xMin and xMax
-        mathX = percentX * (xMax - xMin) + xMin;
-        
-        // define mathY in terms of mathX
-        mathY = evaluateFunction(mathX);
-        percentY = (mathY - yMin) / (yMax - yMin);
-        
-        // transform coordinate so origin is in the bottom left
-        percentY = 1 - percentY;
-        
-        xPixel = percentX * canvas.width;
-        yPixel = percentY * canvas.height;
-        
-        ctx.lineTo(xPixel,yPixel);
-    }
-
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 3;
-    ctx.stroke();
-}
+    equationField.value = expression;
+    equationField.addEventListener('keyup', event => {
+        expression = equationField.value;
+        // setHashFromExpr();
+        graph.drawEquation((x) => {
+            // console.log(evaluateFunction(x));
+            return evaluateFunction(x)
+            // return test(x);
+        }, "black", 3);  
+    });
+}; 
 
 // return the y coordinate in the math space of the given argument number
 function evaluateFunction(arg) {
@@ -86,152 +48,158 @@ function evaluateFunction(arg) {
     return math.evaluate(expression, scope);
 }
 
-function drawLines() {
-    const numVerticalLines = (canvas.width / gridStep);
-    const numHorizontalLines = (canvas.height / gridStep);
-
-    ctx.translate(-yAxisOffset * gridStep, -xAxisOffset * gridStep);
-
-    for (let i = 0; i <= numHorizontalLines; i++) {
-        ctx.beginPath();
-        ctx.lineWidth = 1;
-
-        // if line is x axis draw in different color
-        ctx.strokeStyle = (i === xAxisOffset) ? 'red' : 'gray';
-
-        if (i === numHorizontalLines) {
-            ctx.moveTo(0, gridStep * i);
-            ctx.lineTo(canvas.width, gridStep * i);
-        } else {
-            ctx.moveTo(0, gridStep * i + 0.5);
-            ctx.lineTo(canvas.width, gridStep * i + 0.5);
+class Graph {
+    constructor(con) {
+        // user defined properties  
+        this.canvas = document.getElementById(con.canvasId);
+        this.minX = con.minX;
+        this.minY = con.minY;
+        this.maxX = con.maxX;
+        this.maxY = con.maxY;
+        this.unitsPerTick = con.unitsPerTick;
+        
+        // constants  
+        this.axisColor = "#aaa";
+        this.font = "8pt Calibri";
+        this.tickSize = 20;
+        
+        // relationships  
+        this.context = this.canvas.getContext("2d");
+        this.rangeX = this.maxX - this.minX;
+        this.rangeY = this.maxY - this.minY;
+        this.unitX = this.canvas.width / this.rangeX;
+        this.unitY = this.canvas.height / this.rangeY;
+        this.centerY = Math.round(Math.abs(this.minY / this.rangeY) * this.canvas.height);
+        this.centerX = Math.round(Math.abs(this.minX / this.rangeX) * this.canvas.width);
+        this.iteration = (this.maxX - this.minX) / 1000;
+        this.scaleX = this.canvas.width / this.rangeX;
+        this.scaleY = this.canvas.height / this.rangeY;
+        
+        // draw x and y axis  
+        this.drawXAxis();
+        this.drawYAxis();
+    }
+    drawXAxis() {
+        let context = this.context;
+        context.save();
+        context.beginPath();
+        context.moveTo(0, this.centerY);
+        context.lineTo(this.canvas.width, this.centerY);
+        context.strokeStyle = this.axisColor;
+        context.lineWidth = 2;
+        context.stroke();
+        
+        // draw tick marks  
+        let xPosIncrement = this.unitsPerTick * this.unitX;
+        let xPos, unit;
+        context.font = this.font;
+        context.textAlign = "center";
+        context.textBaseline = "top";
+        
+        // draw left tick marks  
+        xPos = this.centerX - xPosIncrement;
+        unit = -1 * this.unitsPerTick;
+        while (xPos > 0) {
+            context.moveTo(xPos, this.centerY - this.tickSize / 2);
+            context.lineTo(xPos, this.centerY + this.tickSize / 2);
+            context.stroke();
+            context.fillText(unit, xPos, this.centerY + this.tickSize / 2 + 3);
+            unit -= this.unitsPerTick;
+            xPos = Math.round(xPos - xPosIncrement);
         }
-
-        ctx.stroke();
-    }
-
-    for (let i = 0; i <= numVerticalLines; i++) {
-        ctx.beginPath();
-        ctx.lineWidth = 1;
-
-        // if line is x axis draw in different color
-        ctx.strokeStyle = (i === yAxisOffset) ? 'red' : 'gray';
-
-        if (i === numVerticalLines) {
-            ctx.moveTo(gridStep * i, 0);
-            ctx.lineTo(gridStep * i, canvas.height);
-        } else {
-            ctx.moveTo(gridStep * i + 0.5, 0);
-            ctx.lineTo(gridStep * i + 0.5, canvas.height);
+        
+        // draw right tick marks  
+        xPos = this.centerX + xPosIncrement;
+        unit = this.unitsPerTick;
+        while (xPos < this.canvas.width) {
+            context.moveTo(xPos, this.centerY - this.tickSize / 2);
+            context.lineTo(xPos, this.centerY + this.tickSize / 2);
+            context.stroke();
+            context.fillText(unit, xPos, this.centerY + this.tickSize / 2 + 3);
+            unit += this.unitsPerTick;
+            xPos = Math.round(xPos + xPosIncrement);
         }
-
-        ctx.stroke();
+        context.restore();
     }
-
-    ctx.translate(yAxisOffset * gridStep, xAxisOffset * gridStep);
-}
-
-function drawGrid() {
-    const tickMarkWidth = 6;
-
-    const numVerticalLines = (canvas.width / gridStep);
-    const numHorizontalLines = (canvas.height / gridStep);
-
-    drawLines();
-
-    // tick marks along positive x axis
-    for (let i = 1; i < (numVerticalLines - yAxisOffset); i++) {
-        ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = 'black';
-
-        ctx.moveTo(gridStep * i + 0.5, -(tickMarkWidth / 2));
-        ctx.lineTo(gridStep * i + 0.5, tickMarkWidth / 2);
-        ctx.stroke();
-
-        ctx.font = '9px Arial';
-        ctx.textAlign = 'start';
-        ctx.fillText(xAxisStartLabel * i, gridStep * i - 2, 15);
+    drawYAxis() {
+        let context = this.context;
+        context.save();
+        context.beginPath();
+        context.moveTo(this.centerX, 0);
+        context.lineTo(this.centerX, this.canvas.height);
+        context.strokeStyle = this.axisColor;
+        context.lineWidth = 2;
+        context.stroke();
+        
+        // draw tick marks   
+        let yPosIncrement = this.unitsPerTick * this.unitY;
+        let yPos, unit;
+        context.font = this.font;
+        context.textAlign = "right";
+        context.textBaseline = "middle";
+        
+        // draw top tick marks  
+        yPos = this.centerY - yPosIncrement;
+        unit = this.unitsPerTick;
+        while (yPos > 0) {
+            context.moveTo(this.centerX - this.tickSize / 2, yPos);
+            context.lineTo(this.centerX + this.tickSize / 2, yPos);
+            context.stroke();
+            context.fillText(unit, this.centerX - this.tickSize / 2 - 3, yPos);
+            unit += this.unitsPerTick;
+            yPos = Math.round(yPos - yPosIncrement);
+        }
+        
+        // draw bottom tick marks  
+        yPos = this.centerY + yPosIncrement;
+        unit = -1 * this.unitsPerTick;
+        while (yPos < this.canvas.height) {
+            context.moveTo(this.centerX - this.tickSize / 2, yPos);
+            context.lineTo(this.centerX + this.tickSize / 2, yPos);
+            context.stroke();
+            context.fillText(unit, this.centerX - this.tickSize / 2 - 3, yPos);
+            unit -= this.unitsPerTick;
+            yPos = Math.round(yPos + yPosIncrement);
+        }
+        context.restore();
     }
-
-    for (let i = 1; i < numVerticalLines; i++) {
-        ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = 'black';
-
-        ctx.moveTo(-gridStep * i + 0.5, -(tickMarkWidth / 2));
-        ctx.lineTo(-gridStep * i + 0.5, tickMarkWidth / 2);
-        ctx.stroke();
-
-        ctx.font = '9px Arial';
-        ctx.textAlign = 'end';
-        ctx.fillText(-xAxisStartLabel * i, -gridStep * i + 3, 15);
+    drawEquation(equation, color, thickness) {
+        let context = this.context;
+        context.save();
+        this.context.clearRect(0,0,this.canvas.width, this.canvas.height);  
+        this.drawXAxis();
+        this.drawYAxis();
+        // context.save();
+        this.transformContext();
+        
+        context.beginPath();
+        
+        try {
+            context.moveTo(this.minX, equation(this.minX));
+            
+            for (let x = this.minX + this.iteration; x <= this.maxX; x += this.iteration) {
+                context.lineTo(x, equation(x));
+            }
+            
+            context.restore();
+            context.lineJoin = "round";
+            context.lineWidth = thickness;
+            context.strokeStyle = color;
+            context.stroke();
+            context.restore();
+        } catch {
+            context.restore();
+        }     
     }
-
-    // tick marks along positive y axis
-    for (let i = 1; i < (numHorizontalLines - xAxisOffset); i++) {
-        ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = 'black';
-
-        ctx.moveTo(-(tickMarkWidth / 2), gridStep * i + 0.5);
-        ctx.lineTo(tickMarkWidth / 2, gridStep * i + 0.5);
-        ctx.stroke();
-
-        ctx.font = '9px Arial';
-        ctx.textAlign = 'start';
-        ctx.fillText(-yAxisStartLabel * i, 8, gridStep * i + 3);
+    transformContext() {
+        let context = this.context;
+        
+        // move context to center of canvas  
+        context.translate(this.centerX, this.centerY);
+        
+        context.scale(this.scaleX, -this.scaleY);
     }
-
-    for (let i = 1; i < numHorizontalLines; i++) {
-        ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = 'black';
-
-        ctx.moveTo(-(tickMarkWidth / 2), -gridStep * i + 0.5);
-        ctx.lineTo(tickMarkWidth / 2, -gridStep * i + 0.5);
-        ctx.stroke();
-
-        ctx.font = '9px Arial';
-        ctx.textAlign = 'end';
-        ctx.fillText(yAxisStartLabel * i, 15, -gridStep * i + 3);
-    }
-
-    // ctx.translate(-yAxisOffset * gridStep, -xAxisOffset * gridStep);
-
-    // const xPixelStep = canvas.width / numVerticalLines;
-    // const yPixelStep = canvas.height / numHorizontalLines;
-
-    // ctx.beginPath();
-    // for (let x = 0; x < canvas.width; x += xPixelStep) {
-    //     ctx.moveTo(x, 0);
-    //     ctx.lineTo(x, canvas.height)
-    // }
-
-    // for (let y = 0; y < canvas.height; y += yPixelStep) {
-    //     ctx.moveTo(0, y);
-    //     ctx.lineTo(canvas.width, y)
-    // }
-
-    // ctx.strokeStyle = 'gray';
-    // ctx.lineWidth = 1;
-    // ctx.stroke();
-
-    // drawAxes()
-}
-
-// function drawAxes() {
-//     ctx.beginPath();
-//     ctx.moveTo(canvas.width / 2, 0);
-//     ctx.lineTo(canvas.width / 2, canvas.height);
-
-//     ctx.moveTo(0, canvas.height / 2);
-//     ctx.lineTo(canvas.width, canvas.height / 2);
-
-//     ctx.strokeStyle = 'black';
-//     // ctx.lineWidth = 1;
-//     ctx.stroke();
-// }
+}  
 
 // save equation in browser locator hash
 
@@ -259,5 +227,5 @@ function initExprFromHash() {
         setHashFromExpr();
     }
     equationField.value = expression;
-    plot();
+    graph.drawEquation(evaluateFunction, "green", 3);  
 }
